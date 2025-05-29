ba169427cd4c52a188384239aae3635156c7268f
dudwjd1202 <dudwjd1202@gmail.com>
2025-05-29 16:22:35 +0900

    test

---
+++ GGTime_Test/GGTime_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GGTime_Test
+//
+//  Created by 송정범 on 2025/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
